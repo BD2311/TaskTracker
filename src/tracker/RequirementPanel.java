4b9df631c8b64acf704b47a9175662f82882e044
BD2311 <branden.della@gmail.com>
2024-04-30 01:56:14 -0700

    Create RequirementPanel.java

---
+++ src/tracker/RequirementPanel.java
@@ -0,0 +1,26 @@
+package tracker;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class RequirementPanel extends JPanel
+{
+	///// Fields /////
+	
+	private Requirement _requirement;
+	
+	///// Constructor /////
+	
+	public RequirementPanel(Requirement requirement)
+	{
+		this._requirement = requirement;
+	}
+	
+	///// Methods /////
+	
+	private void initializeUI()
+	{
+		
+	}
+}
