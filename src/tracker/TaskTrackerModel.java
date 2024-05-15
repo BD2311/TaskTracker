7377d793257b0293f7fc9f337bb5aac09cb9db9c
BD2311 <branden.della@gmail.com>
2024-05-14 21:57:34 -0700

    Begin creating logic to sort tasks

--- src/tracker/TaskTrackerModel.java
+++ src/tracker/TaskTrackerModel.java
@@ -1,5 +1,8 @@
 package tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Lead Author(s):
  * @author Branden Della bdella@email.com
@@ -12,22 +15,39 @@ package tracker;
 public class TaskTrackerModel 
 {
 	///// Fields /////
-	
+
 	public CategoryPanel planningPanel = new CategoryPanel("Planning");
 	public CategoryPanel inProgressPanel = new CategoryPanel("In Progress");
 	public CategoryPanel finishedPanel = new CategoryPanel("Finished");
-	
+
 	///// Constructor /////
-	
+
 	public TaskTrackerModel()
 	{
-		
+
 	}
-	
+
 	///// Methods /////
 	
-	public void moveCompleteTaskToFinishedPanel(TaskPanel completeTask)
+	public void sortTasks(List<TaskPanel> allTasks)
 	{
+		allTasks = getAllTasks(); // Before sortingTasks get all tasks
 		
+		
+	}
+
+	/**
+	 * 
+	 * 
+	 * @return all tasks within each CategoryPanel
+	 */
+	private List<TaskPanel> getAllTasks()
+	{
+		List<TaskPanel> allTasks = new ArrayList<TaskPanel>();
+		allTasks.addAll(planningPanel.getTasks());
+		allTasks.addAll(inProgressPanel.getTasks());
+		allTasks.addAll(finishedPanel.getTasks());
+		
+		return allTasks;
 	}
 }
