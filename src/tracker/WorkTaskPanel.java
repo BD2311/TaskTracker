e0f009985cfaef3ab533d62527ac5468bacef86f
BD2311 <branden.della@gmail.com>
2024-05-15 14:41:06 -0700

    Update WorkTaskPanel.java

--- src/tracker/WorkTaskPanel.java
+++ src/tracker/WorkTaskPanel.java
@@ -4,14 +4,12 @@ package tracker;
  * Lead Author(s):
  * @author Branden Della bdella@email.com
  * 
- * Version/date: 2024.05.12.001
+ * Version/date: 2024.05.15.001
  * 
  * Responsibilities of class:
  * 
  * Represents a panel displaying a task information and scroll pane of requirements.
- * - Displaying the task header.
- * - Providing functionality to adding requirements
- * - Providing functionality to deleting task
+ * - Use inheritence to construct Task with prefix "Work Task: " +  name
  * 
  */
 @SuppressWarnings("serial")
@@ -29,7 +27,7 @@ public class WorkTaskPanel extends TaskPanel
 	
 	public WorkTaskPanel(String name, CategoryPanel parentCategory, TaskTrackerModel model)
 	{
-		super("Work Task: " + name);
+		super("Work Task: " + name, parentCategory, model);
 	}
 
 	///// Methods /////
