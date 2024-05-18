0d597d58f2fe06424318449d4e7befea251a6f53
BD2311 <branden.della@gmail.com>
2024-05-17 18:29:45 -0700

    Create ModelNotFoundException.java

---
+++ src/tracker/ModelNotFoundException.java
@@ -0,0 +1,21 @@
+package tracker;
+
+/**
+ * Lead Author(s):
+ * @author Branden Della bdella@email.com
+ * 
+ * Version/date: 2024.05.17.001
+ * 
+ * Responsibilities of class:
+ * 
+ * Custom Exception Class
+ * - Flag when a method that gets a model and model is null
+ * 
+ */
+public class ModelNotFoundException extends Exception
+{
+	public ModelNotFoundException()
+	{
+
+	}
+}
